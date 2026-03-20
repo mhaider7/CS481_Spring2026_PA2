a280cf180a38c53aaf15ac3bd0102c77735427dd
mhaider7 <mshaider3@gmail.com>
2026-03-20 00:55:55 -0500

    Preprocessing complete
    What's left is to shuffle, then split the dataset

--- cs481_P02_A20422384.py
+++ cs481_P02_A20422384.py
@@ -1,31 +1,87 @@
 # Names: Hamza Syed, Mazin Haider
 
 import pandas as pd
-import sys
+import sys, re
+from bs4 import BeautifulSoup
+import numpy as np
+import random
 
 #Read in both datasets as pandas df
 fake_df = pd.read_csv("Fake.csv")
 true_df = pd.read_csv("True.csv")
 
 ###Preprocessing
-#Add fake and true columns for each
-fake_df['authenticity'] = 'Fake'
-true_df['authenticity'] = 'True'
+#DATE feature
+#Check for dates
+#for i in fake_df['date'].unique(): print(i, end=" | ")
+#for i in true_df['date'].unique(): print(i, end=" | ")
+#Drop date column
+fake_df.drop(columns=['date'], inplace=True)
+true_df.drop(columns=['date'], inplace=True)
+
+#SUBJECT feature
+#Check for domain of values
+#print(fake_df['subject'].unique())
+#print(true_df['subject'].unique())
+#Output a csv with only headline and subject to manually observe pattern
+#fake_df.to_csv("fake_headline_subject.csv", columns=['title', 'subject'])
+#true_df.to_csv("true_headline_subject.csv", columns=['title', 'subject'])
+#Change 'Government News' to 'politics', 'US_News' to 'News' 'Middle-East' to 'worldnews' in fake_df
+fake_df.loc[fake_df['subject'] == 'Government News', 'subject'] = 'politics'
+fake_df.loc[fake_df['subject'] == 'US_News', 'subject'] = 'News'
+fake_df.loc[fake_df['subject'] == 'Middle-east', 'subject'] = 'worldnews'
+#Change 'politicsNews' to 'politics' in true_df
+true_df.loc[true_df['subject'] == 'politicsNews', 'subject'] = 'politics'
+#Output dataframe to check if all changes went through for subject column
+#fake_df.to_csv("fake_headline_subject_update.csv", columns=['title', 'subject'])
+#true_df.to_csv("true_headline_subject_update.csv", columns=['title', 'subject'])
 
 #Drop duplicate rows
 fake_df = fake_df.drop_duplicates()
 true_df = true_df.drop_duplicates()
 
-#Merge datasets
-df = pd.concat([fake_df, true_df])
+#Combine each instance of the dataframes into a single list
+FAKE = fake_df.values.tolist()
+TRUE = true_df.values.tolist()
+#Break down list elements (title, text, subject) into one long string
+for i in range(len(FAKE)): FAKE[i] = ' '.join(FAKE[i])
+for i in range(len(TRUE)): TRUE[i] = ' '.join(TRUE[i])
 
-#Randomly shuffle datasets between true and false instances
-df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+#Drop dates in text
+for i in range(len(FAKE)): 
+    FAKE[i] = re.sub(r'\d+', '', FAKE[i])
+    FAKE[i] = re.sub(r'January|February|March|April|May|June|July|August|September|October|November|December', '', FAKE[i])
+for i in range(len(TRUE)): 
+    TRUE[i] = re.sub(r'\d+', '', TRUE[i])
+    TRUE[i] = re.sub(r'January|February|March|April|May|June|July|August|September|October|November|December', '', TRUE[i])
 
-print(df.head)
+#Drop html tags (Potentially keep if it makes the prediction better)
+for i in range(len(FAKE)): FAKE[i] = BeautifulSoup(FAKE[i], "html.parser").get_text()
+for i in range(len(TRUE)): TRUE[i] = BeautifulSoup(TRUE[i], "html.parser").get_text()
 
-#Check for odd characters, emoji's
+#Drop social media handlers
+for i in range(len(FAKE)): FAKE[i] = re.sub(r'@[^\s]+', '', FAKE[i])
+for i in range(len(TRUE)): TRUE[i] = re.sub(r'@[^\s]+', '', TRUE[i])
 
+#note: Pattern matching here takes a long time to compute
+#Drop punctuation (parens, comma, period, exclamation, question)
+for i in range(len(FAKE)): FAKE[i] = re.sub(r'"|!|\.|\?|\(|\)|\[|\]|,|\\|\/|;|:', '', FAKE[i])
+for i in range(len(TRUE)): TRUE[i] = re.sub(r'"|!|\.|\?|\(|\)|\[|\]|,|\\|\/|;|:', '', TRUE[i])
+
+#Lower-case everything
+for i in range(len(FAKE)): FAKE[i] = FAKE[i].lower()
+for i in range(len(TRUE)): TRUE[i] = TRUE[i].lower()
+
+#Add fake and true element for each list
+for i in range(len(FAKE)): FAKE[i] = [FAKE[i]]; FAKE[i].append('fake')
+for i in range(len(TRUE)): TRUE[i] = [TRUE[i]]; TRUE[i].append('true')
+
+#Merge fake and true lists
+DATA = FAKE + TRUE
+
+#Convert to dataframe
+
+#Shuffle dataframe
 
 ###Command line arguments
 # Algo : 0 = Naive Bayes | 1 = k-NN
@@ -38,4 +94,6 @@ else:
     TRAIN_SIZE = int(sys.argv[2])
     TEST_SIZE = 100 - TRAIN_SIZE
 
-print(ALGO, TRAIN_SIZE, TEST_SIZE)
+#print(ALGO, TRAIN_SIZE, TEST_SIZE)
+
+###Data split
